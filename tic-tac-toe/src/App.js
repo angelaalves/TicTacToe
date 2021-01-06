6adb9a7eb26afed30ba5c2c482c53e153aca538d
angelaalves <angelapatriciafilipealves@gmail.com>
2021-01-06 13:26:25 +0000

    WIP

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import React, {Component} from 'react';
 class App extends Component{
+
+  constructor(){
+    this.state = {
+      board : Array(9).fill("X")
+    }
+  }
   render(){
+
+    const box= this.state.board.map(box=>{<div className="box">{box}</div>})
   return (
     <div className="container">
     <h1>Tic Tac Toe</h1>
     <div className="board">
-    <div class-name="box"></div>
-    <div class-name="box"></div>
-    <div class-name="box"></div>
-    <div class-name="box"></div>
-    <div class-name="box"></div>
-    <div class-name="box"></div>
-    <div class-name="box"></div>
-    <div class-name="box"></div>
-    <div class-name="box"></div>
+  
+{box}
     </div>
     </div>
   );}
