d347d7b8d4ae40804d90fc96776bfbf663eec3ea
angelaalves <angelapatriciafilipealves@gmail.com>
2021-01-06 12:32:06 +0000

    WIP:Created the app

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, {Component} from 'react';
+class App extends Component{
+  render(){
+  return (
+    <div className="container">
+    <h1>Tic Tac Toe</h1>
+    <div className="board">
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    <div class-name="box"></div>
+    </div>
+    </div>
+  );}
+}
+
+export default App;
